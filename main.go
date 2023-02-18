197d8c618d7612ed24fad11397ed2cdc0df950cb
yuichiyasui <yassan.b.a.w@gmail.com>
2023-02-18 17:55:00 +0900

    Response json

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
+type Task struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, HTTP Server!")
+	task := &Task{Id: "1", Name: "GoでWeb APIを作る"}
+
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	if err := enc.Encode(task); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprint(w, buf.String())
 }
 
 func main() {
