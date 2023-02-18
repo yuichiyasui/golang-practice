f8305d1d692fec5dbbda59c750133620dfe69719
yuichiyasui <yassan.b.a.w@gmail.com>
2023-02-18 20:28:30 +0900

    Divide files

--- main.go
+++ main.go
@@ -1,41 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
 )
 
-type Task struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
-}
-
-func handler(writer http.ResponseWriter, req *http.Request) {
-	msg := req.FormValue("msg")
-	if msg != "" {
-		fmt.Println(req.FormValue("msg"))
-	}
-	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-
-	task := &Task{Id: "1", Name: "GoでWeb APIを作る"}
-	var buf bytes.Buffer
-	enc := json.NewEncoder(&buf)
-	if err := enc.Encode(task); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Fprint(writer, buf.String())
-}
-
-func oldTodoHandler(writer http.ResponseWriter, req *http.Request) {
-	http.Redirect(writer, req, "/todo", http.StatusMovedPermanently)
-}
-
 func main() {
-	http.HandleFunc("/todo", handler)
-	http.HandleFunc("/todo-old", oldTodoHandler)
+	http.HandleFunc("/todo", Handler)
+	http.HandleFunc("/todo-old", OldTodoHandler)
 	http.ListenAndServe(":8080", nil)
 }
