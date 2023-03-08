83710baa7ea188454a69f63093d2a3418538c01e
yuichiyasui <yassan.b.a.w@gmail.com>
2023-03-08 23:05:05 +0900

    Init db

--- app/handlers.go
+++ app/handlers.go
@@ -1,35 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-type Task struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
-}
-
-func Handler(writer http.ResponseWriter, req *http.Request) {
-	msg := req.FormValue("msg")
-	if msg != "" {
-		fmt.Println(req.FormValue("msg"))
-	}
-	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-
-	task := &Task{Id: "1", Name: "GoでWeb APIを作る"}
-	var buf bytes.Buffer
-	enc := json.NewEncoder(&buf)
-	if err := enc.Encode(task); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Fprint(writer, buf.String())
-}
-
-func OldTodoHandler(writer http.ResponseWriter, req *http.Request) {
-	http.Redirect(writer, req, "/todo", http.StatusMovedPermanently)
+func TodoPostHandler(ctx *gin.Context) {
+	println("Create task!")
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Create task!",
+	})
 }
--- app/main.go
+++ app/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.POST("/task", func(ctx *gin.Context) {
-		println("Create task!")
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Create task!",
-		})
-	})
+	router.POST("/task", TodoPostHandler)
 
 	router.Run()
 }
