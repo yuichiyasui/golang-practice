3ede9cf583cbafa3951743b2c113c1af35e61709
yuichiyasui <yassan.b.a.w@gmail.com>
2023-03-16 01:55:01 +0900

    Fix task schema

--- app/models/task.go
+++ app/models/task.go
@@ -6,18 +6,18 @@ import (
 	"gorm.io/gorm"
 )
 
-type status string
+type Status string
 
 const (
-	todo       status = "todo"
-	inProgress status = "in_progress"
-	done       status = "done"
+	todo       Status = "todo"
+	inProgress Status = "in_progress"
+	done       Status = "done"
 )
 
 type Task struct {
 	ID        uint           `json:"id" gorm:"primaryKey"`
 	Title     string         `json:"title" gorm:"not null"`
-	Status    status         `json:"status" sql:"type:status; default: 'todo'; not null"`
+	Status    Status         `json:"status" sql:"type:status" gorm:"default: 'todo'; not null"`
 	CreatedAt time.Time      `json:"createdAt"`
 	UpdatedAt time.Time      `json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `json:"deletedAt" gorm:"index"`
